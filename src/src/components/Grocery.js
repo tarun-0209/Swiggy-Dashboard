d2851ac725d2cc93f8d773834c13fd15702771b6
tarun-0209 <tarunpurohit2903@gmail.com>
2024-05-16 19:58:58 +0530

    Delete src directory

--- src/src/components/Grocery.js
+++
@@ -1,4 +0,0 @@
-const Grocery = () => {
-  return <h1>Grocery Page</h1>;
-};
-export default Grocery;
--- src/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const CDN_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const LOGO_URL =
-  "https://cdn2.downdetector.com/static/uploads/logo/download-6_wRAOMlb.png";
-export const MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.32750&lng=78.03250&restaurantId=";
